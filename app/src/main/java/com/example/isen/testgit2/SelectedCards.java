d13c6eb7b86e1f455138994dac141851352aa1a6
Tanguy MERCIER <tanguy.mercier@isen-lille.fr>
2015-01-21 16:46:11 +0100

    q"e-e,dnyn

--- app/src/main/java/com/example/isen/testgit2/SelectedCards.java
+++ app/src/main/java/com/example/isen/testgit2/SelectedCards.java
@@ -4,14 +4,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 
 public class SelectedCards extends ActionBarActivity {
+
+    private ListView selectedCards;
+    private ListView handCards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_cards);
+
+        selectedCards = (ListView) findViewById(R.id.listViewSelectedCards);
+        handCards = (ListView) findViewById(R.id.listViewHandCards);
+
     }
 
 
